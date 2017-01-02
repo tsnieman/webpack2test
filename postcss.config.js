e1cc1fbd6e52b258dfc50b9bc4d7a9097ece89f5
tyler <tyler@tsnieman.net>
2017-01-02 01:52:38 -0800

    add note/comment/placeholder

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   plugins: [
+    /* TODO use custom css modules scoped name (i.e. generated class name) ...
+    * currently the problem is getting babel-plugin-react-css-modules to
+    * also be this same scoped name */
+    /*
+    require('postcss-modules')({
+      generateScopedName: function(name, filename, css) {
+        var path  = require('path');
+        var i     = css.indexOf('.' + name);
+        var line  = css.substr(0, i).split(/[\r\n]/).length;
+        var file  = path.basename(filename, '.css');
+
+        return '_' + file + '_' + line + '_' + name;
+      }
+    }),
+    */
     require('postcss-color-function')(),
     require('autoprefixer')({ browsers: ['last 2 versions'] }),
   ]
