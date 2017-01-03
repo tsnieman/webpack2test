e5544786b9a6b90aa5ba532fc72082b11c6cab66
tyler <tyler@tsnieman.net>
2017-01-02 23:02:49 -0800

    resolve webpack modules paths when linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": "./config/webpack.config.js",
+      }
+    }
+  }
 };
