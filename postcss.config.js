2a20f2cd299d5c750448fa188978ff5333a81042
tyler <tyler@tsnieman.net>
2017-01-06 10:06:42 -0800

    add useful postcss plugins (nested, import, custom-properties)

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,13 @@ module.exports = {
       }
     }),
     */
+    require('postcss-nested')(),
+    require('postcss-import')({
+      path: [
+        './src'
+      ],
+    }),
+    require('postcss-custom-properties')(),
     require('postcss-color-function')(),
     require('autoprefixer')({ browsers: ['last 2 versions'] }),
   ]
