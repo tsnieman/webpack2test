66f8f6f3f474f8d2aa40eae9ed75ea9d8c2c491d
tyler <tyler@tsnieman.net>
2017-02-05 15:13:13 -0800

    add https server (by default)

--- README.md
+++ README.md
@@ -13,6 +13,22 @@ Made this to test Webpack 2. It's a little React app. It's not the most minimal 
 - Jest
 - ESLint (w/ AirBnB config)
 
+# Using the local https server
+
+By default, the app is configured to redirect all traffic to HTTPS. As such, you will probably need to generate some SSL certs for the app to use:
+
+- Generate SSL certificates: `openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365`
+	- **NOTE**: when you see `Common Name (e.g. server FQDN or YOUR name) []:`, specify `localhost`.
+- Set the cert password as an environment variable: `SSL_CERT_PASS=yourSslCertPass yarn run start:dev`
+- Add the cert to Chrome to avoid warnings about it: [http://stackoverflow.com/a/15076602](http://stackoverflow.com/a/15076602)
+
+# Environment variables
+
+| key           | values | description                                          |
+|---------------|------------------------------------------------------|-----|
+| SSL_CERT_PASS | n/a | The password for the localhost https server's certs. |
+| NODE_ENV | `production` `test` `development` | Stuff |
+
 # Issues
 
 ## Some stuff doesn't hot reload 'til 2nd re-build
@@ -23,3 +39,11 @@ Found a github issue that seems to be tracking this:
 
 > "So to sum up, hot reloading system.imported code would only work on the second time the webpack builds."
 via  https://github.com/gaearon/react-hot-loader/issues/303#issuecomment-253836872
+
+## Chrome warns of 'non-secure connection' when navigating to `https://localhost` (local https server)
+
+The cert is self-signed, just for this repo. Add the cert to Chrome: [http://stackoverflow.com/a/15076602](http://stackoverflow.com/a/15076602)
+
+## I don't want to deal with HTTPS
+
+Then disable the 'redirect to HTTPS' in the dev server.
--- devServer.js
+++ devServer.js
@@ -1,17 +1,37 @@
 var path = require('path');
+var fs = require('fs');
+var https = require('https');
 var webpack = require('webpack');
 var express = require('express');
+
+var HTTP_PORT = 4200;
+var HTTPS_PORT = 8443;
+
 var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
-var config = require('./config/webpack.config');
 
+var ensureSecureMiddleware = function ensureSecureMiddleware(req, res, next){
+  if(req.secure){
+    // OK, continue
+    return next();
+  };
+  // handle port numbers if you need non defaults
+  res.redirect(`https://${req.hostname}:${HTTPS_PORT}${req.url}`); // express 4.x
+};
+
+// Setup webpack
+var webpackConfig = require('./config/webpack.config');
+var compiler = webpack(webpackConfig);
+
+// Setup the app.
+// --------------
 var app = express();
-var compiler = webpack(config);
 
-var PORT = 4200
+// Redirect to HTTPS
+app.all('*', ensureSecureMiddleware); // keep at top of routing calls
 
 app.use(devMiddleware(compiler, {
-  publicPath: config.output.publicPath, // where bundles live
+  publicPath: webpackConfig.output.publicPath, // where bundles live
 
   historyApiFallback: true,
 
@@ -30,10 +50,29 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'built', 'index.html'));
 });
 
-app.listen(PORT, function (err) {
+// Launch http server.
+// ---------------------------
+app.listen(HTTP_PORT, function (err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log('Listening at http://localhost:' + PORT);
+  console.log('Listening at http://localhost:' + HTTP_PORT);
+});
+
+// Launch https server.
+// ---------------------------
+var sslOptions = {
+	key: fs.readFileSync('./key.pem'),
+	cert: fs.readFileSync('./cert.pem'),
+	passphrase: process.env.SSL_CERT_PASS
+};
+
+var httpsServer = https.createServer(sslOptions, app);
+httpsServer.listen(HTTPS_PORT, function (err) {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log('Listening at https://localhost:' + HTTPS_PORT);
 });
