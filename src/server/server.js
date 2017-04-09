c73fb50a9c250f75c3fa101065320c9d67a0aea4
tyler <tyler@tsnieman.net>
2017-04-09 15:53:06 -0700

    remove https from express (prefer handling https w/ nginx)

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,7 @@
 /* eslint-disable prefer-template */
 
-import fs from 'fs';
 import path from 'path';
 import spdy from 'spdy';
-import http from 'http';
 
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -15,12 +13,10 @@ import webpackConfig from '../../config/webpack.config';
 
 import {
   HTTP_PORT,
-  HTTPS_PORT,
 } from '../constants/server';
 
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import httpsEverywhereMiddleware from './middleware/httpsEverywhere';
 import securityMiddlewares from './middleware/security';
 import compressionMiddleware from 'compression';
 
@@ -42,9 +38,6 @@ app.use(...securityMiddlewares);
 
 // Compression middleware
 app.use(compressionMiddleware());
-
-// Redirect to HTTPS
-app.all('*', httpsEverywhereMiddleware); // keep at top of routing calls
 
 if (process.env.NODE_ENV === 'development') {
   // Dev middleware
@@ -79,94 +72,16 @@ app.use('/public/images/favicon.png', express.static(path.join(__dirname, '/../.
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: 'public/built' });
 });
-/*
-app.get('*', (req, res) => {
-  const requestUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-
-  const clientOptions = {
-    initialLocation: req.path,
-  };
-
-  const theClient = evalBundleCode(requestUrl).default(clientOptions);
-  const appBody = renderToString(theClient);
-
-  // "you have to make sure to call rewind on server, or you'll get a memory leak."
-  // https://github.com/nfl/react-helmet#server-usage
-  const head = Helmet.rewind();
-
-  const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
-
-  res.send(`
-<html>
-  <head ${head.htmlAttributes.toString()}>
-    <meta name=viewport content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/png" href="/public/images/favicon.png"/>
-    ${head.title.toString()}
-    ${head.meta.toString()}
-    ${head.link.toString()}
-    ${head.base.toString()}
-
-    ${
-      Object.keys(assetsByChunkName)
-        .map((name) => {
-          const chunkPath = webpackConfig.output.publicPath + assetsByChunkName[name];
-
-          if (chunkPath.endsWith('.css')) {
-            return `<link rel="stylesheet" href="${chunkPath}"></link>`;
-          }
-
-          return '';
-        }).join('\n    ') // prevents comma
-    }
-  </head>
-  <body>
-    <div id="app-index">${appBody}</div>
-
-    ${
-      Object.keys(assetsByChunkName)
-        .map((name) => {
-          const chunkPath = webpackConfig.output.publicPath + assetsByChunkName[name];
-
-          if (chunkPath.endsWith('.js')) {
-            return `<script src="${chunkPath}"></script>`;
-          }
-
-          return '';
-        }).join('\n    ') // prevents comma.
-    }
-  </body>
-</html>
-  `);
-});
-*/
 
 // Launch http server.
 // ---------------------------
-http
+spdy
   .createServer(app)
   .listen(HTTP_PORT, (err) => {
-    if (err) {
-      return console.error(err); // eslint-disable-line no-console
-    }
-
-    return console.log(`Listening at http://localhost:${HTTP_PORT}`); // eslint-disable-line no-console
-  });
-
-// Launch https server.
-// ---------------------------
-const sslOptions = {
-  key: fs.readFileSync(`${__dirname}/key.pem`),
-  cert: fs.readFileSync(`${__dirname}/cert.pem`),
-  passphrase: process.env.SSL_CERT_PASS,
-};
-
-spdy
-  .createServer(sslOptions, app)
-  .listen(HTTPS_PORT, (err) => {
     if (err) {
       console.error(err); // eslint-disable-line no-console
       return process.exit(1);
     }
 
-    return console.log(`Listening at https://localhost:${HTTPS_PORT}`); // eslint-disable-line no-console
+    return console.log(`Listening at http://localhost:${HTTP_PORT}`); // eslint-disable-line no-console
   });
