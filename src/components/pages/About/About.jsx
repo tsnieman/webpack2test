a3aa55063eb00e6f537dbc400fd4a898cf2be9fb
tsnieman <tyler@tsnieman.net>
2017-04-13 19:58:30 -0700

    about page styling

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -4,28 +4,20 @@ import React from 'react';
 import './About.css';
 
 import Message from 'components/base/Message';
+import Typography from 'components/Typography';
 import GithubUser from 'containers/GithubUser';
+
+import { PLAINTEXT_NAME } from 'constants/app';
 
 const About = () => (
   <div styleName="wrapper">
-    <h1>About page!</h1>
-    <p>Contributors.</p>
-    <p>Also a good chance to just use some APIs to build some test fetching/loading/whatever.</p>
+    <Typography>
+      <h1>About {PLAINTEXT_NAME}</h1>
+      <h2>Contributors</h2>
+      <p>Also a good chance to just use some APIs to build some test fetching/loading/whatever.</p>
+    </Typography>
 
-    <Message.Wrapper variant="info">
-      <Message.Body>
-        Note: loading intentionally delayed (less than 2 seconds) to show off the
-        fancy loading effect.
-      </Message.Body>
-    </Message.Wrapper>
-
-    <Message.Wrapper variant="info">
-      <Message.Body>
-        Note: also one of the 'humans' in the list is a fake github username to
-        show a failure-handling state. This is mostly to see how smooth I can
-        make failure.
-      </Message.Body>
-    </Message.Wrapper>
+    <br />
 
     <ul>
       <li styleName="human">
@@ -36,6 +28,25 @@ const About = () => (
         <GithubUser username="definitelyafakeuserwithaloginthatistoolong" />
       </li>
     </ul>
+
+    <br />
+
+    <Message.Wrapper variant="info">
+      <Message.Body>
+        Note: loading intentionally delayed (less than 2 seconds) to show off the
+        fancy loading effect.
+      </Message.Body>
+    </Message.Wrapper>
+
+    <br />
+
+    <Message.Wrapper variant="info">
+      <Message.Body>
+        Note: also one of the 'humans' in the list is a fake github username to
+        show a failure-handling state. This is mostly to see how smooth I can
+        make failure.
+      </Message.Body>
+    </Message.Wrapper>
   </div>
 );
 
