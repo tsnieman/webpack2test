346a882502199b2132bc3a6b230a8a4b6fa0af8d
tsnieman <tyler@tsnieman.net>
2017-04-15 16:55:49 -0700

    add silly animated waves

--- src/components/app/Header/Header.jsx
+++ src/components/app/Header/Header.jsx
@@ -6,21 +6,25 @@ import { Link } from 'react-router-dom';
 import { PLAINTEXT_NAME } from 'constants/app';
 
 const Header = () => (
-  <header styleName="wrapper">
-    <strong styleName="brand"><Link to="/">{PLAINTEXT_NAME}</Link></strong>
+  <div>
+    <header styleName="wrapper">
+      <strong styleName="brand"><Link to="/">{PLAINTEXT_NAME}</Link></strong>
 
-    <nav>
-      <ul styleName="nav-items">
-        <li>
-          <Link to="/about">/about</Link>
-        </li>
+      <nav>
+        <ul styleName="nav-items">
+          <li>
+            <Link to="/about">/about</Link>
+          </li>
 
-        <li>
-          <Link to="/404-fake-route">/404-fake-route</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+          <li>
+            <Link to="/404-fake-route">/404-fake-route</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div styleName="wave" />
+  </div>
 );
 
 Header.propTypes = {
